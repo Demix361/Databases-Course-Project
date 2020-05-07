cc1f912322171be6b89b87c936562d76ccbf96fb
Demix361 <ti-aminov@yandex.ru>
2020-05-07 23:44:54 +0300

    tutorial 5

--- furniture_shop/blog/views.py
+++ furniture_shop/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Demix',
-        'title': 'Blog post 1',
-        'content': 'Helllllllllllllllooooooooooo!!!!!!!!!!!!!!!!!',
-        'date_posted': 'May 7, 2020'
-    },
-    {
-        'author': 'Who',
-        'title': 'Blog post 2',
-        'content': 'HUHUHUAHDIuha!!',
-        'date_posted': 'May 8, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
