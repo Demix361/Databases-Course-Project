9ef37d1bbe386e206b2b71c551a7fa22ce0ed6ae
Demix361 <ti-aminov@yandex.ru>
2020-05-08 17:19:49 +0300

    tutorial 7

--- furniture_shop/users/views.py
+++ furniture_shop/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -10,8 +11,13 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
