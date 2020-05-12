7339de9845c7c387099576e512b225a052540725
Demix361 <ti-aminov@yandex.ru>
2020-05-13 01:49:06 +0300

    tutorial 11

--- furniture_shop/users/models.py
+++ furniture_shop/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
+import cv2
 
 
 class Profile(models.Model):
@@ -10,12 +10,18 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        img = cv2.imread(self.image.path)
+        h = img.shape[0]
+        w = img.shape[1]
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        if h > w:
+            img = img[(h - w) // 2:h - (h - w - (h - w) // 2), :]
+        else:
+            img = img[:, (w - h) // 2:w - (w - h - (w - h) // 2)]
+
+        img = cv2.resize(img, (300, 300), interpolation=cv2.INTER_AREA)
+
+        cv2.imwrite(self.image.path, img)
