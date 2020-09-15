914372e586b66064cd55dea162c6ff1024720f94
Demix361 <ti-aminov@yandex.ru>
2020-09-15 11:37:55 +0300

    wip

--- shop_v4/shop/views.py
+++ shop_v4/shop/views.py
@@ -44,7 +44,7 @@ def filter_products(options, category_id):
     for i in res:
         print(i.product.name)
 
-
+'''
 class ProductCategoryDetailView(DetailView):
     model = Category
     template_name = 'shop/productcategory_list.html'
@@ -63,12 +63,13 @@ class ProductCategoryDetailView(DetailView):
         context['filtered_products'] = Product.objects.filter(category=self.get_object())
         context['colors'] = Color.objects.all()
 
-        #all_q = self.get_object().get_products()
+
         filtered_q = FeatureSet.objects.filter(product__category=self.get_object())
+        # Массив примененных фильтров
         used = []
 
+        # Если продукт имеет features
         if len(filtered_q) != 0:
-
             # Заполненение filtered_q отфильтрованными элементами по Feature
             for f in self.get_object().get_features().filter(type='checkbox'):
                 cur_filtered = FeatureSet.objects.filter(product=9999999999)
@@ -82,14 +83,11 @@ class ProductCategoryDetailView(DetailView):
                             cur_filtered = cur_filtered | FeatureSet.objects.filter(product=line.product)
                     else:
                         used[-1].append(0)
-
-                print(used[-1])
+                # Если хотя бы один фильтр был применен, возвращаем отфильтрованные строки
                 if 1 in used[-1]:
                     filtered_q = filtered_q & cur_filtered
 
-
-
-            # Заполненение filtered_q отфильтрованными элементами по Color
+            # применение фильтров по цвету
             cur_filtered = FeatureSet.objects.filter(product=9999999999)
             used = []
             for clr in Color.objects.all():
@@ -116,9 +114,106 @@ class ProductCategoryDetailView(DetailView):
                 final_products = final_products | Product.objects.filter(id=i)
 
             context['filtered_products'] = final_products
+        # Если продукт не имеет features
         else:
             used = []
             cur_products = Product.objects.filter(name='000000000000000000')
+
+            for clr in Color.objects.all():
+                var = self.request.GET.get('clr_' + str(clr.id))
+
+                if is_valid_queryparam(var):
+                    used.append(1)
+                    cur_products = cur_products | Product.objects.filter(color=clr.id)
+                else:
+                    used.append(0)
+
+            if 1 in used:
+                context['filtered_products'] = cur_products
+            else:
+                context['filtered_products'] = self.get_object().get_products()
+
+        return context
+'''
+
+
+class ProductCategoryDetailView(DetailView):
+    model = Category
+    template_name = 'shop/productcategory_list.html'
+    context_object_name = 'category'
+    paginate_by = 18
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Список товаров в корзине
+        if not self.request.user.is_anonymous:
+            context['products_in_cart'] = Cart.objects.filter(
+                user=self.request.user).first().cartitem_set.all().values_list('product',
+                                                                                       flat=True)
+
+        #context['filtered_products'] = Product.objects.filter(category=self.get_object())
+
+        # Список цветов
+        context['colors'] = Color.objects.all()
+
+
+        filtered_q = FeatureSet.objects.filter(product__category=self.get_object())
+        # Массив примененных фильтров
+        used = []
+
+        # Если продукт имеет features
+        if len(filtered_q) != 0:
+            # Заполненение filtered_q отфильтрованными элементами по Feature
+            for f in self.get_object().get_features().filter(type='checkbox'):
+                cur_filtered = FeatureSet.objects.none()
+                used.append([])
+                for v in f.get_variants():
+                    var = self.request.GET.get('ch_' + str(v.id))
+
+                    if is_valid_queryparam(var):
+                        used[-1].append(1)
+                        for line in FeatureSet.objects.filter(feature_variant=v.id):
+                            cur_filtered = cur_filtered | FeatureSet.objects.filter(product=line.product)
+                    else:
+                        used[-1].append(0)
+                # Если хотя бы один чекбокс в фильтре был отмечен, используем AND с прошлым результатом
+                if 1 in used[-1]:
+                    filtered_q = filtered_q & cur_filtered
+
+            # применение фильтров по цвету
+            cur_filtered = FeatureSet.objects.none()
+            used = []
+            for clr in Color.objects.all():
+                var = self.request.GET.get('clr_' + str(clr.id))
+
+                if is_valid_queryparam(var):
+                    used.append(1)
+                    for line in FeatureSet.objects.filter(product__color=clr.id):
+                        cur_filtered = cur_filtered | FeatureSet.objects.filter(product=line.product)
+                else:
+                    used.append(0)
+
+            if 1 in used:
+                filtered_q = filtered_q & cur_filtered
+
+            # FeatureSet -> Product
+            ok_products = []
+            for p in filtered_q.values('product'):
+                ok_products.append(p['product'])
+            ok_products = set(ok_products)
+
+            final_products = Product.objects.none()
+            for i in ok_products:
+                if i in filtered_q.values('product'):
+                    print(i, 'ok')
+                final_products = final_products | Product.objects.filter(id=i)
+
+            context['filtered_products'] = final_products
+        # Если продукт не имеет features
+        else:
+            used = []
+            cur_products = Product.objects.none()
 
             for clr in Color.objects.all():
                 var = self.request.GET.get('clr_' + str(clr.id))
