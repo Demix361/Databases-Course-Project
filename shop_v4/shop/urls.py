1ee5d1ae636de10fadc719e09cae2ffef2610c56
Demix361 <49446810+Demix361@users.noreply.github.com>
2020-09-30 15:41:54 +0300

    cleaned

--- shop_v4/shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ProductListView, ProductDetailView, ProductCategoryDetailView
-
-
-urlpatterns = [
-    path('', ProductListView.as_view(), name='shop-home'),
-    path('category/<int:pk>/', ProductCategoryDetailView.as_view(), name='shop-category'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='shop-product')
-]
